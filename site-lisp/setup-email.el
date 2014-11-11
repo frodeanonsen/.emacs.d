3783537a5e89e350f55879b386500c6c17e0bf10
Frode Ånonsen <frode@Frodes-MacBook-Pro.local>
2014-11-11 10:40:29 +0100

    New email settings

--- site-lisp/setup-email.el
+++ site-lisp/setup-email.el
@@ -11,27 +11,27 @@
      (mu4e-drafts-folder "/Anonsen/[Gmail].Drafts")
      (mu4e-refile-folder "/Anonsen/Archive")
      (user-mail-address "frode@anonsen.org"))
-    ("Springmotion"
-     (mu4e-sent-folder "/Springmotion/[Gmail].Sent Mail")
-     (mu4e-drafts-folder "/Springmotion/[Gmail].Drafts")
-     (mu4e-refile-folder "/Springmotion/Archive")
-     (user-mail-address "frode.anonsen@springmotion.com"))))
+    ("Knowit"
+     (mu4e-sent-folder "/Knowit/[Gmail].Sent Mail")
+     (mu4e-drafts-folder "/Knowit/[Gmail].Drafts")
+     (mu4e-refile-folder "/Knowit/Archive")
+     (user-mail-address "frode.anonsen@knowit.no"))))
 
 ;; Trash
 (setq mu4e-trash-folder
       (lambda (msg)
         (cond
-         ((or (mu4e-message-contact-field-matches msg :to "@springmotion")
-              (mu4e-message-contact-field-matches msg :cc "@springmotion"))
-          "/Springmotion/[Gmail].Trash")
+         ((or (mu4e-message-contact-field-matches msg :to "@knowit")
+              (mu4e-message-contact-field-matches msg :cc "@knowit"))
+          "/Knowit/[Gmail].Trash")
          ;; important to have a catch-all at the end!
          (t  "/Anonsen/[Gmail].Trash"))))
 
 ;; Shortcuts
 (setq mu4e-maildir-shortcuts
       '(("/Anonsen/INBOX" . ?a)
-        ("/Springmotion/INBOX" . ?s)
-	("/Anonsen/_Hanse" . ?h)
+        ("/Knowit/INBOX" . ?k)
+	("/Anonsen/D5L" . ?d)
 	("/readme" . ?r)))
 
 ;; sending mail
@@ -59,8 +59,8 @@
 	      (cond
 	       ((or (string-match "@anonsen.org" from)
 		    (string-match "@anonsen.org" cc)) "anonsen")
-	       ((or (string-match "@springmotion.com" from)
-		    (string-match "@springmotion.com" cc)) "springmotion")
+	       ((or (string-match "@knowit.no" from)
+		    (string-match "@knowit.no" cc)) "knowit")
 	       (t "gmail"))))
 	  (setq message-sendmail-extra-arguments (list '"-a" account))))))
 (setq message-sendmail-envelope-from 'header)
@@ -74,34 +74,34 @@
 	      (if msg
 		  (setq user-mail-address
 			(cond
-			 ((mu4e-message-contact-field-matches msg :to "@springmotion")
-			  "frode.anonsen@springmotion.com")
+			 ((mu4e-message-contact-field-matches msg :to "@knowit")
+			  "frode.anonsen@knowit.no")
 			 ((mu4e-message-contact-field-matches msg :to "@anonsen.org")
 			  "frode@anonsen.org")
 			 (t "frode@anonsen.org")))))))
 
 (setq mu4e-user-mail-address-list
-      (list "frode@anonsen.org" "frode.anonsen@springmotion.com"))
+      (list "frode@anonsen.org" "frode.aanonsen@knowit.no"))
 
 
 ;; Dynamic select email accounts when composing new message
 (defun my-mu4e-set-account ()
-       "Set the account for composing a message."
-       (let* ((account
-               (if mu4e-compose-parent-message
-                   (let ((maildir (mu4e-message-field mu4e-compose-parent-message :maildir)))
-                     (string-match "/\\(.*?\\)/" maildir)
-                     (match-string 1 maildir))
-                 (completing-read (format "Compose with account: (%s) "
-                                          (mapconcat #'(lambda (var) (car var)) my-mu4e-account-alist "/"))
-                                  (mapcar #'(lambda (var) (car var)) my-mu4e-account-alist)
-                                  nil t nil nil (caar my-mu4e-account-alist))))
-              (account-vars (cdr (assoc account my-mu4e-account-alist))))
-         (if account-vars
-             (mapc #'(lambda (var)
-                       (set (car var) (cadr var)))
-                   account-vars)
-           (error "No email account found"))))
+  "Set the account for composing a message."
+  (let* ((account
+          (if mu4e-compose-parent-message
+              (let ((maildir (mu4e-message-field mu4e-compose-parent-message :maildir)))
+                (string-match "/\\(.*?\\)/" maildir)
+                (match-string 1 maildir))
+            (completing-read (format "Compose with account: (%s) "
+                                     (mapconcat #'(lambda (var) (car var)) my-mu4e-account-alist "/"))
+                             (mapcar #'(lambda (var) (car var)) my-mu4e-account-alist)
+                             nil t nil nil (caar my-mu4e-account-alist))))
+         (account-vars (cdr (assoc account my-mu4e-account-alist))))
+    (if account-vars
+        (mapc #'(lambda (var)
+                  (set (car var) (cadr var)))
+              account-vars)
+      (error "No email account found"))))
 
 (add-hook 'mu4e-compose-pre-hook 'my-mu4e-set-account)
 
@@ -115,7 +115,7 @@
 ;; Maildirs ext
 (require 'mu4e-maildirs-extension)
 (setq mu4e-maildirs-extension-custom-list
-      '( "/Anonsen/INBOX" "/Springmotion/INBOX" ))
+      '( "/Anonsen/INBOX" "/Knowit/INBOX" ))
 (mu4e-maildirs-extension)
 
 (provide 'setup-email)
