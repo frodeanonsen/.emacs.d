4574a0a3c64d44ab4e007d1a86f68a1fbd7eccf3
Frode Ånonsen <frode@Frodes-MacBook-Pro.local>
2014-11-23 22:16:55 +0100

    Use 2 spaces for indent

--- site-lisp/setup-web-mode.el
+++ site-lisp/setup-web-mode.el
@@ -13,5 +13,10 @@
 (setq web-mode-engines-alist
       '(("php"    . "\\.phtml\\'")))
 
+(defun my-web-mode-hook ()
+  "Hooks for Web mode."
+  (setq web-mode-markup-indent-offset 2))
+
+(add-hook 'web-mode-hook  'my-web-mode-hook)
 
 (provide 'setup-web-mode)
