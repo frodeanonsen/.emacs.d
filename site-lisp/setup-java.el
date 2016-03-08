eca6fafdfc7d6e7541297a12d8f913a1f939f46d
Frode Ånonsen <frode@anonsen.org>
2016-03-08 15:05:19 +0100

    Java... wip

--- site-lisp/setup-java.el
+++ site-lisp/setup-java.el
@@ -6,21 +6,23 @@
 ;;; Code:
 (require 'use-package)
 
-(use-package emacs-eclim
-  :ensure t
-  :config (progn
-            (global-eclim-mode)
-            (ac-emacs-eclim-config)
-            (custom-set-variables
-             '(eclim-eclipse-dirs '("/Applications/Eclipse.app/Contents/Eclipse"))
-             '(eclim-executable "/Applications/Eclipse.app/Contents/Eclipse/eclim"))
-            (setq java-basic-offset 2
-                  tab-width 2
-                  indent-tabs-mode nil)))
+;; (use-package emacs-eclim
+;;   :ensure t
+;;   :config (progn
+;;             (global-eclim-mode)
+;;             (ac-emacs-eclim-config)
+;;             (custom-set-variables
+;;              '(eclim-eclipse-dirs '("/Applications/Eclipse.app/Contents/Eclipse"))
+;;              '(eclim-executable "/Applications/Eclipse.app/Contents/Eclipse/eclim"))
+;;             (setq java-basic-offset 2
+;;                   tab-width 2
+;;                   indent-tabs-mode nil)))
 
-(require 'eclim)
-(require 'eclimd)
-(require 'ac-emacs-eclim-source)
+;; (require 'eclim)
+;; (require 'eclimd)
+;; (require 'ac-emacs-eclim-source)
+
+(use-package groovy-mode :ensure t)
 
 (provide 'setup-java)
 ;;; setup-java.el ends here
